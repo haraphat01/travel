6fe74cd86d0e12800cf2a54f7be6cc77185a3a3f
Draxster <118171977+Draxster@users.noreply.github.com>
2023-07-09 19:27:21 +0300

    Add tests

--- test.py
+++ test.py
@@ -1,18 +1,45 @@
+from utils import division, summa
 import kb
 import handlers
-
-
 import pytest
 
-from aiogram_tests import MockedBot
-import aiogram.utils
-from aiogram_tests.handler import MessageHandler
-from aiogram_tests.types.dataset import MESSAGE
+import text
 
+async def test_division():
+   assert division(10,2) == 5
 
-@pytest.mark.asyncio
-async def test_echo():
-    request = MockedBot(MessageHandler(handlers.start_handler))
-    calls = await request.query(message=MESSAGE.as_object(text="Choose the language: ", reply_markup=kb.language_menu))
-    answer_message = calls.send_messsage.fetchone()
-    assert answer_message.text == "Choose the language: "
+async def test_summa():
+   assert summa(3, 2) == 5
+
+async def test_start_handler():
+    assert handlers.start_handler("Choose the language: ")
+
+async def test_ru():
+    assert handlers.language_confirmation_ru("Choose the language: ")
+
+async def test_eng():
+    assert handlers.language_confirmation_eng("Choose the language: ")
+
+async def test_profile_search():
+    assert handlers.profile_search("Before I send you options for places you can move to, I need to ask a few questions.")
+
+async def test_next_handler():
+    assert handlers.next("1. Input your gender", kb.gender_menu)
+
+async def test_women():
+    assert handlers.women("2. Why do you want to move to another country?")
+
+async def test_man():
+    assert handlers.man("2. Why do you want to move to another country?")
+
+async def test_less_2k():
+    assert handlers.less_2k("3. What is your budget?")
+
+async def test_from_1_to_3():
+    assert handlers.from_1_to_3("3. What is your budget?")
+
+async def test_greater_3k():
+    assert handlers.from_1_to_3("3. What is your budget?")
+
+async def test_alone():
+    assert handlers.alone("5. Have you decided where you want to relocate?")
---
+++ utils.py
@@ -0,0 +1,6 @@
+def division(a, b):
+    return a / b
+
+
+def summa(a, b):
+    return a + b
